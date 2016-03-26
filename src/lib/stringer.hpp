0b83c792fca787a97f4ea3de113180caf5610db3
Andy Hall <anhall@adobe.com>
2016-03-25 21:05:37 -0600

    Use fold expression to simplify lib::stringer()

--- src/lib/stringer.hpp
+++ src/lib/stringer.hpp
@@ -8,45 +8,11 @@
 
 namespace lib {
 
-    namespace detail {
-
-        template < typename Stream >
-        using IsStream =
-                std::is_base_of<
-                    std::ios_base,
-                    std::remove_reference_t<Stream>
-                >;
-
-    }
-
-    template <
-        typename Stream,
-
-        typename = std::enable_if_t< detail::IsStream<Stream>::value >
-    >
-    std::ostream & streamer( Stream &&stream ) {
-        return stream;
-    }
-
-    template <
-        typename Stream,
-        typename Head,
-        typename ...Args,
-
-        typename = std::enable_if_t< detail::IsStream<Stream>::value >
-    >
-    std::ostream & streamer( Stream &&stream, Head &&head, Args && ...args ) {
-        return streamer(
-                stream << std::forward<Head>( head ),
-                std::forward<Args>( args )... );
-    }
-
-    template < typename Head, typename ...Args >
-    std::string stringer( Head &&head, Args && ...args ) {
-        return streamer(
-                std::ostringstream(),
-                std::forward<Head>( head ),
-                std::forward<Args>( args )... ).str();
+    template< typename... Args >
+    std::string stringer( Args &&...args ) {
+        std::ostringstream oss;
+        ( oss << ... << args );
+        return oss.str();
     }
 
 }
